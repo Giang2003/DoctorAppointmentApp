86b4e911e0d78fb4fa3260572af460589c4d6937
Ngo Phuong Nam <nam.ngo.210101@student.fulbright.edu.vn>
2023-12-05 17:47:37 +0700

    Add all the files of the Doctor App

---
+++ app/src/main/java/com/example/badapp/LoginActivity.java
@@ -0,0 +1,107 @@
+package com.example.badapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.*;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+
+public class LoginActivity extends AppCompatActivity {
+    ProgressBar progressBar;
+    TextInputLayout passwordLayout, usernameLayout;
+    EditText editUsername, editPassword;
+    Button loginButton;
+    TextView registerText;
+    FirebaseAuth authProfile;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        //
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+         usernameLayout = findViewById(R.id.usernameInputLayout);
+         progressBar = findViewById(R.id.progressBar);
+         editUsername = findViewById(R.id.usernameInputEditText);
+         editPassword = findViewById(R.id.passwordInputEditText);
+         loginButton = (Button) findViewById(R.id.registerButton);
+         registerText = findViewById(R.id.textSignUp);
+         registerText.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+         passwordLayout = findViewById(R.id.passwordInputLayout);
+         authProfile = FirebaseAuth.getInstance();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editUsername.getText().toString();
+                String password = editPassword.getText().toString();
+
+                if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
+                    editUsername.setError("Email and password required");
+                    editUsername.requestFocus();
+                }
+                else{
+                    progressBar.setVisibility(View.VISIBLE);
+                    loginUser(email,password);
+//                    startActivity(new Intent(LoginActivity.this,HomeActivity.class));
+//                    Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        //Opens the Register page
+        registerText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+
+    private void loginUser(String email, String password) {
+        authProfile.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this,new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    //Check the role of user
+                    //if: patient:
+                    startActivity(new Intent(LoginActivity.this,HomeActivity.class));
+                    //if: doc:
+
+                    Toast.makeText(LoginActivity.this, "Login Successful",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    try{
+                        throw task.getException();
+                    } catch (FirebaseAuthInvalidCredentialsException e){
+                        usernameLayout.setError("Wrong username or password. Please try again");
+                        passwordLayout.requestFocus();
+                    } catch (Exception e){
+                        Log.e("Login Acitivity",e.getMessage());
+
+                    }
+                    //Toast.makeText(LoginActivity.this, "Something went wrong",Toast.LENGTH_SHORT).show();
+                }
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/badapp/RegisterActivity.java
@@ -0,0 +1,173 @@
+package com.example.badapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.*;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Calendar;
+
+public class RegisterActivity extends AppCompatActivity {
+    private EditText editTextFullname, editTextBirthDate, editTextPhone, editTextPassword, editTextEmail;
+    private TextView loginText;
+
+    private Button registerButton;
+    private DatePickerDialog picker;
+    private Spinner genderSpinner ;
+            //= findViewById(R.id.genderSpinner);
+    private ArrayAdapter<CharSequence> adapter;
+                    //= ArrayAdapter.createFromResource(this, R.array.genders,android.R.layout.simple_spinner_item);
+
+    private ProgressBar progressBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        genderSpinner = findViewById(R.id.genderSpinner);
+        adapter = ArrayAdapter.createFromResource(this, R.array.genders,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(adapter);
+        loginText = findViewById(R.id.loginText);
+        editTextFullname = findViewById(R.id.editTextRegisterFullname);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextBirthDate = findViewById(R.id.editTextBirthDate);
+        editTextPhone = findViewById((R.id.editTextPhoneNumber));
+        editTextPassword = findViewById(R.id.editTextPassword);
+        registerButton = findViewById(R.id.registerButton);
+        progressBar = findViewById(R.id.progressBar);
+        loginText.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+        editTextBirthDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(calendar.YEAR);
+
+                picker = new DatePickerDialog(RegisterActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        editTextBirthDate.setText(dayOfMonth+"/"+ (month+1) + "/" + year);
+                    }
+                }, year,month,day);
+                picker.show();
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Obtain the data entered
+
+                String fullName = editTextFullname.getText().toString();
+                String email = editTextEmail.getText().toString();
+                String birthdate = editTextBirthDate.getText().toString();
+                String phone = editTextPhone.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                if (TextUtils.isEmpty(fullName)){
+                    editTextFullname.setError("Fullname is required");
+                    editTextFullname.requestFocus();
+                }
+
+                else if (TextUtils.isEmpty(email)){
+                    editTextEmail.setError("Email is required");
+                    editTextEmail.requestFocus();
+                }
+
+                else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    editTextEmail.setError("Please enter a valid email");
+                    editTextEmail.requestFocus();
+                }
+
+                else if (TextUtils.isEmpty(birthdate)){
+                    editTextBirthDate.setError("Birthdate is required");
+                    editTextBirthDate.requestFocus();
+                }
+                else if (TextUtils.isEmpty(phone)){
+                    editTextPhone.setError("Phone number is required");
+                    editTextPhone.requestFocus();
+                }
+
+                else if(TextUtils.isEmpty(password)){
+                    editTextPassword.setError("Passwords cannot be empty");
+                    editTextPassword.requestFocus();
+                }
+
+                else if(password.length() < 8){
+                    editTextPassword.setError("Passwords must be longer than 8 characters");
+                    editTextPassword.requestFocus();
+                }
+                else{
+                    progressBar.setVisibility(View.VISIBLE);
+                    //Toast.makeText(getApplicationContext(),"Register success",Toast.LENGTH_SHORT).show();
+                    registerUser(fullName,email,birthdate,phone,password);
+                }
+            }
+        });
+
+
+        loginText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+    }
+
+    private void registerUser(String fullName, String email, String birthdate, String phone, String password) {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegisterActivity.this,
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this,"User registered successfully", Toast.LENGTH_SHORT).show();
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                            firebaseUser.sendEmailVerification();
+
+                            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else{
+                            try {
+                                throw task.getException();
+                            } catch (FirebaseAuthInvalidCredentialsException e){
+                                editTextEmail.setError("Your email is invalid or already in use. Please use a different email");
+                                editTextEmail.requestFocus();
+                            } catch (FirebaseAuthUserCollisionException e){
+                                editTextEmail.setError("A user is already registered with the given email. Please use a different email");
+                                editTextEmail.requestFocus();
+                            } catch (Exception e){
+                                Log.e("Register Activity",e.getMessage());
+                                Toast.makeText(RegisterActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                        progressBar.setVisibility(View.GONE);
+                    }
+
+                });
+    }
+}
